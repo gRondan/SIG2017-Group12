74c616d5a19173faea6c531d425b638bc59ecddd
gRondan <grondan93@gmail.com>
2017-10-28 15:26:23 -0300

    url token

--- CensusCar/WebContent/js/censusCar.js
+++ CensusCar/WebContent/js/censusCar.js
@@ -4,19 +4,20 @@ var mapView
 var mapURL = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"
 var clientId = "mbeKLJKeIY6J2Gyz"
 var clientSecret = "ad9ed9d489b341988779aed7870e2b86"
+var tokenURL = "https://www.arcgis.com/sharing/oauth2/token?client_id="+clientId+"&grant_type=client_credentials&client_secret="+clientSecret+"&f=pjson"
 var responseToken
-var url = 
+var url =
 
-require([
-  "esri/Map",
-  "esri/views/MapView",
-  "esri/layers/TileLayer",
-  "esri/widgets/Search",
-  "esri/tasks/Locator",
-  "dojo/dom",
-  "dojo/on",
-  "dojo/domReady!"],
-  function (Map, MapView, Tiled, Search, Locator, dom, on, domReady) {
+  require([
+    "esri/Map",
+    "esri/views/MapView",
+    "esri/layers/TileLayer",
+    "esri/widgets/Search",
+    "esri/tasks/Locator",
+    "dojo/dom",
+    "dojo/on",
+    "dojo/domReady!"],
+    function (Map, MapView, Tiled, Search, Locator, dom, on, domReady) {
 
 
       var points = [];
@@ -33,72 +34,72 @@ require([
       getToken();
 
       tiled = new Tiled(mapURL);
-      
+
       map = new Map({
-        layers: [tiled]   
+        layers: [tiled]
       });
 
-    mapView = new MapView({
-      container: "map",
-      zoom: 4,
-      center: [-95, 39],
-      spatialReference: { wkid: 102100 },
-      map: map
-    });
-    
-    
-    //FUNCIONES AUXILIARES
-    
-    function getToken(){
-      $.ajax({
-        type: "POST",
-        url: "https://www.arcgis.com/sharing/oauth2/token?client_id=mbeKLJKeIY6J2Gyz&grant_type=client_credentials&client_secret=ad9ed9d489b341988779aed7870e2b86&f=pjson",
-        //data: data,
-        success: (response) => {
-          console.log(response);
-          responseToken = response.access_token;
-      },
-        dataType: "json",
-        async: false
+      mapView = new MapView({
+        container: "map",
+        zoom: 4,
+        center: [-95, 39],
+        spatialReference: { wkid: 102100 },
+        map: map
       });
-    }
+
+
+      //FUNCIONES AUXILIARES
+
+      function getToken() {
+        $.ajax({
+          type: "POST",
+          url: tokenURL,
+          //data: data,
+          success: (response) => {
+            console.log(response);
+            responseToken = response.access_token;
+          },
+          dataType: "json",
+          async: false
+        });
+      }
 
       var searchWidget = new Search({
-        view: mapView, 
+        view: mapView,
         sources: [{
-            locator: new Locator({ url: "//geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer" }),
-            singleLineFieldName: "SingleLine",
-            placeholder: "Find location in USA",
-            countryCode: "US"
+          locator: new Locator({ url: "//geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer" }),
+          singleLineFieldName: "SingleLine",
+          placeholder: "Find location in USA",
+          countryCode: "US"
         }]
       });
-      mapView.ui.add(searchWidget, {position: "top-left", index: 0});
+      mapView.ui.add(searchWidget, { position: "top-left", index: 0 });
 
-      searchWidget.on("select-result", function(event){
+      searchWidget.on("select-result", function (event) {
         console.log("The selected search result: ", event);
       });
       searchWidget.on("select-result", (result) => {
         var point = {
-            name: result.result.name,
+          name: result.result.name,
+          geometry: result.result.feature.geometry,
+          symbol: pointMarker,
+          graphic: new Graphic({
             geometry: result.result.feature.geometry,
             symbol: pointMarker,
-            graphic: new Graphic({
-                geometry: result.result.feature.geometry, 
-                symbol: pointMarker,
-                spatialReference: { wkid: 102100 }, 
-                // Atributos para el servidor de eventos
-                attributes: {
-                    event_type: "17", 
-                    description: result.result.name
-                }
-            })
+            spatialReference: { wkid: 102100 },
+            // Atributos para el servidor de eventos
+            attributes: {
+              event_type: "17",
+              description: result.result.name
+            }
+          })
         };
         addPoint(point);
         console.clear();
         console.log("The selected search result: ", points[0], points[1]);
       });
 
-      function addPoint(point){
+      function addPoint(point) {
         // Establece el id
         point.id = points.length + 1;
 
@@ -110,7 +111,7 @@ require([
         // Cambios en View
         //updateStopsList();
       }
-      
+
       function getToken() {
         $.ajax({
           type: "POST",
@@ -124,4 +125,4 @@ require([
           async: false
         });
       }
-  });
+    });
