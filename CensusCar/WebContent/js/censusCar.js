c168785e46e4f0452d88494d6f525bda9d4552de
gRondan <grondan93@gmail.com>
2017-10-28 19:00:22 -0300

    hay ruta

--- CensusCar/WebContent/js/censusCar.js
+++ CensusCar/WebContent/js/censusCar.js
@@ -2,11 +2,22 @@ var map;
 var tiled
 var mapView
 var mapURL = "http://services.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer"
-var clientId = "mbeKLJKeIY6J2Gyz"
-var clientSecret = "ad9ed9d489b341988779aed7870e2b86"
-var tokenURL = "https://www.arcgis.com/sharing/oauth2/token?client_id="+clientId+"&grant_type=client_credentials&client_secret="+clientSecret+"&f=pjson"
+var clientId = "56pgncO7oT1NYKoY"
+var clientSecret = "65dd33cc13224721b1849d2e8c32381a"
+var tokenURL = "https://www.arcgis.com/sharing/oauth2/token?client_id=" + clientId + "&grant_type=client_credentials&client_secret=" + clientSecret + "&f=pjson"
 var responseToken
-var url =
+var url
+var routeURL = "https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World?token="
+var points = []
+var pointsLayer
+var routesLayer
+var countiesLayer
+
+var routeGraphic = {
+  type: "simple-line",
+  color: "grey",
+  width: 4,
+};
 
 require([
   "esri/Map",
@@ -18,10 +29,13 @@ require([
   "esri/tasks/Locator",
   "dojo/dom",
   "dojo/on",
-  "dojo/domReady!"],
-  function (Map, MapView, Tiled, Graphic, GraphicsLayer, Search, Locator, dom, on, domReady) {
+  "dojo/domReady!",
+  "esri/tasks/RouteTask",
+  "esri/tasks/support/RouteParameters",
+  "esri/tasks/support/FeatureSet"],
+  function (Map, MapView, Tiled, Graphic, GraphicsLayer, Search, Locator, dom, on, domReady, RouteTask, RouteParameters, FeatureSet) {
 
-    var points = [];
+
 
     //Marcador de los puntos buscados
     var pointMarker = {
@@ -37,18 +51,15 @@ require([
 
     //Capa Tiled pedida en letra de obligatorio
     tiled = new Tiled(mapURL);
-    
+
     //Creación del mapa
     map = new Map({
-      layers: [tiled]   
+      layers: [tiled]
     });
 
-    //capa de puntos buscados
-    var pointsLayer = new GraphicsLayer({
-      title: "Puntos",
-      id: "pointsLayer"
-    });
-    map.layers.add(pointsLayer);
+    setLayers();
+
+
 
     mapView = new MapView({
       container: "map",
@@ -57,50 +68,49 @@ require([
       spatialReference: { wkid: 102100 },
       map: map
     });
-    
-    
-    
 
 
-     
+
+
+
+
 
     //Creación del buscador
     var searchWidget = new Search({
-      view: mapView, 
+      view: mapView,
       sources: [{
-          locator: new Locator({ url: "//geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer" }),
-          singleLineFieldName: "SingleLine",
-          placeholder: "Find location in USA",
-          countryCode: "US"
+        locator: new Locator({ url: "//geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer" }),
+        singleLineFieldName: "SingleLine",
+        placeholder: "Find location in USA",
+        countryCode: "US"
       }]
     });
-    mapView.ui.add(searchWidget, {position: "top-left", index: 0});
+    mapView.ui.add(searchWidget, { position: "top-left", index: 0 });
 
     //Evento que devuelve la búsqueda realizada
     searchWidget.on("select-result", (result) => {
       var point = {
-          name: result.result.name,
+        name: result.result.name,
+        geometry: result.result.feature.geometry,
+        symbol: pointMarker,
+        graphic: new Graphic({
           geometry: result.result.feature.geometry,
           symbol: pointMarker,
-          graphic: new Graphic({
-              geometry: result.result.feature.geometry, 
-              symbol: pointMarker,
-              spatialReference: { wkid: 102100 }, 
-              // Atributos para el servidor de eventos
-              attributes: {
-                  event_type: "17", 
-                  description: result.result.name
-              }
-          })
+          spatialReference: { wkid: 102100 },
+          // Atributos para el servidor de eventos
+          attributes: {
+            event_type: "17",
+            description: result.result.name
+          }
+        })
       };
       addPoint(point);
-      console.clear();
       console.log("The selected search result: ", points);
     });
 
-     //FUNCIONES AUXILIARES
+    //FUNCIONES AUXILIARES
 
-     function getToken() {
+    function getToken() {
       $.ajax({
         type: "POST",
         url: tokenURL,
@@ -114,8 +124,8 @@ require([
       });
     }
 
-    function addPoint(point){
-      
+    function addPoint(point) {
+
       point.id = points.length + 1;
 
       // Agrega el punto a la capa de puntos
@@ -123,8 +133,55 @@ require([
 
       points.push(point);
 
+      if (points.length === 3) {
+        calculateRoute();
+      }
+
       // Cambios en View
       //updateStopsList();
     }
-    
-});
+
+    function calculateRoute() {
+      RouteParameters = new RouteParameters({
+        stops: new FeatureSet(),
+        outSpatialReference: { wkid: 102100 }
+      })
+      for (i = 0; i < points.length; i++) {
+        RouteParameters.stops.features.push(points[i].graphic);
+      };
+      RouteTask = new RouteTask({
+        //url: "https://route.arcgis.com/arcgis/rest/services/World/Route/NAServer/Route_World?token=fwk1izMxKT3KCiBHhrzmkM73UyyyB93oR9lOHtAKxneem7CwWgGgUsAfxjDEycDd_OFvNMjcsDd6oZEMZKv11VueLAWIgs5Cjkf6YWp6qJjqsmYIBit86uCjP5UY_7gzTYfoowjOZmo4YYxyBUR8QQ"
+        url : routeURL + responseToken
+      })
+      console.log(routeURL+ responseToken);
+      var RouteResoults = RouteTask.solve(RouteParameters)
+      .then((data) => {
+        var routeResult = data.routeResults[0].route;
+        routeResult.symbol = routeGraphic;
+        routesLayer.removeAll();
+        routesLayer.add(routeResult);
+
+        current_route = routeResult;
+    })
+    }
+    function setLayers() {
+      pointsLayer = new GraphicsLayer({
+        title: "Directions",
+        id: "pointsLayer"
+      });
+      map.layers.add(pointsLayer);
+
+      routesLayer = new GraphicsLayer({
+        title: "Routes",
+        id: "routesLayer"
+      });
+      map.layers.add(routesLayer);
+
+      countiesLayer = new GraphicsLayer({
+        title: "Counties",
+        id: "countiesLayer"
+      });
+      map.layers.add(countiesLayer);
+
+    }
+  });
