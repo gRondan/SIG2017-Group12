ee9a4c971b60cc89f54775876320930251090999
Rodrigo Liñares <r93linares@gmail.com>
2017-10-28 12:40:27 -0300

    Search y token

--- CensusCar/WebContent/js/censusCar.js
+++ CensusCar/WebContent/js/censusCar.js
@@ -14,7 +14,19 @@ require([
   "dojo/domReady!"],
   function (Map, MapView, Tiled, Search, Locator, dom, on, domReady) {
 
-   
+      var points = [];
+
+      var pointMarker = {
+        type: "simple-marker",
+        color: [226, 119, 40],
+        outline: {
+          color: [255, 255, 255],
+          width: 2
+        }
+      };
+
+      getToken();
+
       tiled = new Tiled(mapURL);
       
       map = new Map({
@@ -39,8 +51,55 @@ require([
         }]
       });
       mapView.ui.add(searchWidget, {position: "top-left", index: 0});
-      
-      function getToken(){
 
+      searchWidget.on("select-result", function(event){
+        console.log("The selected search result: ", event);
+      });
+      searchWidget.on("select-result", (result) => {
+        var point = {
+            name: result.result.name,
+            geometry: result.result.feature.geometry,
+            symbol: pointMarker,
+            graphic: new Graphic({
+                geometry: result.result.feature.geometry, 
+                symbol: pointMarker,
+                spatialReference: { wkid: 102100 }, 
+                // Atributos para el servidor de eventos
+                attributes: {
+                    event_type: "17", 
+                    description: result.result.name
+                }
+            })
+        };
+        addPoint(point);
+        console.clear();
+        console.log("The selected search result: ", points[0], points[1]);
+      });
+
+      function addPoint(point){
+        // Establece el id
+        point.id = points.length + 1;
+
+        // Agrega a la capa de paradas
+        //stopsLyr.add(point.graphic);
+
+        points.push(point);
+
+        // Cambios en View
+        //updateStopsList();
+      }
+      
+      function getToken() {
+        $.ajax({
+          type: "POST",
+          url: "https://www.arcgis.com/sharing/oauth2/token?client_id=mbeKLJKeIY6J2Gyz&grant_type=client_credentials&client_secret=ad9ed9d489b341988779aed7870e2b86&f=pjson",
+          //data: data,
+          success: (response) => {
+            console.log(response);
+            responseToken = response.access_token;
+          },
+          dataType: "json",
+          async: false
+        });
       }
   });
